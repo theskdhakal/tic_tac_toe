70fc78a88477e7a001cd0cc73c59f0df4cfae9e6
Shiva Dhakal <theskdhakal@gmail.com>
2023-02-02 23:53:45 +1100

    some more

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const O_Text = "O";
 const X_Text = "X";
 let currentPlayer = X_Text;
 let spaces = Array(9).fill(null);
+
+const startGame = () => {
+  boxes.forEach((box) => box.addEventListener("click", boxclicked));
+};
+
+function boxclicked(e) {
+  const id = e.target.id;
+
+  if (!space[id]) {
+    spaces[id] == currentPlayer;
+  }
+}
+
+startGame();
