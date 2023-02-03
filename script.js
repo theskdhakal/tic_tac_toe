a0a9ca712c2daafe05c209f8d9463616b98d5a41
Shiva Dhakal <theskdhakal@gmail.com>
2023-02-03 12:27:59 +1100

    tic tac toe completed

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let playerText = document.getElementById("playerText");
 let restartBtn = document.getElementById("restartBtn");
 let boxes = Array.from(document.getElementsByClassName("box"));
 
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue(
+  "--winning-block"
+);
 const O_Text = "O";
 const X_Text = "X";
 let currentPlayer = X_Text;
@@ -14,9 +17,54 @@ const startGame = () => {
 function boxclicked(e) {
   const id = e.target.id;
 
-  if (!space[id]) {
-    spaces[id] == currentPlayer;
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
+
+    if (playerHasWon() !== false) {
+      playerText = `${currentPlayer} has won!`;
+
+      let winning_blocks = playerHasWon();
+
+      winning_blocks.map(
+        (box) => (boxes[box].style.backgroundColor = winnerIndicator)
+      );
+      return;
+    }
+
+    currentPlayer = currentPlayer == X_Text ? O_Text : X_Text;
   }
 }
 
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+function playerHasWon() {
+  for (const condition of winningCombos) {
+    let [a, b, c] = condition;
+
+    if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+      return [a, b, c];
+    }
+  }
+  return false;
+}
+restartBtn.addEventListener("click", restart);
+
+function restart() {
+  spaces.fill(null);
+
+  boxes.forEach((box) => {
+    box.innerText = "";
+
+    box.style.backgroundColor = "";
+  });
+}
 startGame();
